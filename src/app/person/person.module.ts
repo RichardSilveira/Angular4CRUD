96c0ace3f4e17e44d2914f0c38f9d554a399074b
Gerson Pedroso <gersonpedroso14@gmail.com>
2017-04-04 09:56:09 -0400

    person-edit

--- src/app/person/person.module.ts
+++ src/app/person/person.module.ts
@@ -7,13 +7,15 @@ import {PersonEditComponent} from './person-edit/person-edit.component';
 import {PersonRoutingModule} from './person-routing.module';
 import {PersonService} from './shared/person.service';
 import {DataTableModule} from 'primeng/components/datatable/datatable';
+import {MessagesModule} from 'primeng/primeng';
 import {GenderPipe} from './shared/gender.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     PersonRoutingModule,
-    DataTableModule
+    DataTableModule,
+    MessagesModule
   ],
   declarations: [PersonDashboardComponent, PersonDetailsComponent, PersonEditComponent, GenderPipe],
   providers: [PersonService]
--- src/app/person/shared/person.service.ts
+++ src/app/person/shared/person.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Person } from './person.model';
 import { PERSONS } from './mock-person';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/filter';
 
 @Injectable()
 export class PersonService {
@@ -15,5 +16,18 @@ export class PersonService {
       observer.next(PERSONS);
       }, 2000);
     });
+  }
+
+  getPersonForId(id: number): Observable<Person[]> {
+
+    if(id <= 0){
+      return Observable.throw('The id parameter can not be less than or equal to zero');
+    }
+
+    return new Observable(observer => {
+      setTimeout(() => {
+      observer.next(PERSONS);
+      }, 2000);
+    }).filter((x: Person) => x.id === id);
   }
 }
